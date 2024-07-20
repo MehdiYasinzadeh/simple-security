e01d3acb67d22ec286ecc9f086384facdd1cc928
mehdi-yasinzadeh <yasin.mehdi.my@gmail.com>
2024-07-21 00:07:27 +0330

    add readme.md file for app and add some services and filter for jwt token

---
+++ src/main/java/ir/yasinzadeh/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,34 @@
+package ir.yasinzadeh.security.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (authorizationHeader == null && !authorizationHeader.startsWith("Bearer ")) {
+            return;
+        }
+        jwt = authorizationHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+    }
+}
---
+++ src/main/java/ir/yasinzadeh/security/config/JwtService.java
@@ -0,0 +1,40 @@
+package ir.yasinzadeh.security.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "288cf0d016c64fbf06b2f4d702dace82ad85de05eeb842cc59650a73ff25ed07";
+
+    public String extractUsername(String jwt) {
+        return null;
+    }
+
+    public<T> T extractClaim(String token, Function<Claims, T> claimsResolver)
+    {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
